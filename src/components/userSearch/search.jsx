e1ff941a0f1cb76c05877d515f73dc4f36eb1922
CallMeLux <Jourdanjeff@yahoo.com>
2022-12-19 13:16:23 -0500

    Figured out dynamic search for users

--- src/components/userSearch/search.jsx
+++ src/components/userSearch/search.jsx
@@ -2,47 +2,56 @@ import { useRef, useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 
+//the component function
 const Search = () => {
     const [user, setUser] = useState([])
     const id = useRef()
-    
 
+    //I need to now grab user input to place into id to load that particular user.
+
+    {/*this is how a comment is done in a jsx file*/}
+
+
+    //the actual function that will do the search
     const fetchUsers = () => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
+        axios.get(`https://jsonplaceholder.typicode.com/users?id=${id.current.value}`)
         .then(response => {
             setUser(response.data)
         })
     }
 
 
+    //for page refresh
     useEffect(() => {
        
     }, [])
 
     return (
-        
-        <div>
+        //attempting to style and format the information
+
+        <>
+            <input placeholder="1-10" ref={id}></input>
             <button onClick={fetchUsers}>Search Users</button>
             {user.length > 0 && (
                 <ul>
                     {user.map(user => (
                         <li key = {user.id}> 
-                            <table>
+                            
                             <tr><th>{user.name}</th></tr>
                         
-                            <tb>{user.address.street}</tb>
+                            <td>{user.address.street}</td>
                             <td>{user.email}</td>
-                            </table>
+                            
                         </li>
                 ))}
                 </ul>
             )}
-        </div>
+        </>
     )
 
 }
 
-
+//search component complete, need to learn to style it better.
 
 
 export default Search
