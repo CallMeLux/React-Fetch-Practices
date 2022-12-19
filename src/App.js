2b6d07d11f29b645193c4abd73968e2388e2a361
CallMeLux <Jourdanjeff@yahoo.com>
2022-12-19 15:31:27 -0500

    Cannot get count of completed tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Search from './components/userSearch/search';
-import TodoList from './components/todoList/todoList';
-import { useState } from 'react';
 
 function App() {
- 
-  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <Search />
-      <TodoList todos={todos} />
     </div>
   );
 }
--- src/components/userSearch/search.jsx
+++ src/components/userSearch/search.jsx
@@ -5,7 +5,12 @@ import { useEffect } from "react";
 //the component function
 const Search = () => {
     const [user, setUser] = useState([])
+    const [todos,setTodos] = useState([])
+    const [count, setCount] = useState(0)
     const id = useRef()
+
+    //for todos
+    
 
     //I need to now grab user input to place into id to load that particular user.
 
@@ -20,6 +25,28 @@ const Search = () => {
         })
     }
 
+
+    //a function to fetch todos based on the current user
+    const fetchTodos = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/todos?userId=${id.current.value}`)
+        .then(response => {
+            setTodos(response.data)
+        })
+    }
+
+
+
+    //this function does not work currently.
+    const countCompleted = () => {
+ 
+        if(todos.completed === true){
+            setCount(count => count + 1)
+        }
+    }
+
+
+    //need a function to use a conditional to update count based on completed todos. 
+    //or, I may need the context of the todos to get an accurante count.
 
     //for page refresh
     useEffect(() => {
@@ -46,7 +73,25 @@ const Search = () => {
                 ))}
                 </ul>
             )}
+
+                        <button onClick={fetchTodos}>Check Todos</button>
+                        {todos.length > 0 && (
+                            <ul>
+                                {todos.map(todos => (
+                                    <li key = {todos.id}>
+                                        {todos.title}
+                                        <span>
+                                            <input type="checkbox" checked={todos.completed} />
+                                        </span>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+
+
+
         </>
+        
     )
 
 }
