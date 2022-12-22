0c43c91c709bde8cac4ac076b02a49e3d0c39814
Jeffrey Jourdan <jeffrey.jourdan@ad.infosys.com>
2022-12-22 11:15:45 -0500

    Messing with themes and css for better formatting.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { deepPurple, grey } from '@mui/material/colors';
 import { style } from '@mui/system';
 import styled from 'styled-components';
 import './App.css';
 import Search from './components/userSearch/search';
 
+const theme = createTheme({
+  palette: {
+    // values for page theme
+    background: {
+      default: grey[900],
+      paper: '101010',
+      grid: grey[900]
+    },
+    text: {
+      primary: '#fff',
+      secondary: deepPurple,
+    },
+
+  },
+})
+
+
 
 function App() {
   return (
+    <ThemeProvider theme = {theme}>
+      <CssBaseline />
     <div className="App">
+      <center>
       <Search />
+      </center>
     </div>
+    </ThemeProvider>
   );
 }
 
